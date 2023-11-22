0fa6fed23ef4a539de971b190b66c4353db9f9b2
matiassaade <matiassaade@gmail.com>
2023-11-22 08:31:12 -0300

    primer commit

---
+++ carrito.js
@@ -0,0 +1,10 @@
+// Crear un carrito de compras. 
+// ■ Crear una aplicación de carrito de compras donde se ingresen
+// productos como elementos en un array.
+// ■ las tareas que debe hacer son las siguientes:
+// ● Agregar producto al carrito
+// ● Listar los productos del carrito
+// ● Buscar un producto en el carrito
+// ● Filtrar productos que coincidan con una palabra, parte de ella
+// o una letra
+// ● Eliminar producto del carrito
