949c28e1a0452d9e36e42f4183adca4d53dc4505
matiassaade <matiassaade@gmail.com>
2023-11-22 09:30:05 -0300

    funcion agrgar

--- carrito.js
+++ carrito.js
@@ -8,3 +8,16 @@
 // ● Filtrar productos que coincidan con una palabra, parte de ella
 // o una letra
 // ● Eliminar producto del carrito
+// const carrito = ['nafta', 'pan', 'aceite', 'gaseosa', 'gas']
+
+const agregarProducto = (item) =>{
+    let producto = item
+    if(carrito.find(item => item == producto)){
+        console.log('el producto ya se encuentra en el carrito')
+    }else{
+        carrito.push(producto)
+        console.log('Producto agregado correctamente')
+    }
+}
+agregarProducto('carne')
+console.log(carrito)
