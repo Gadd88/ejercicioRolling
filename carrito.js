4ef42f15325131580869aef5079f62e9fcf5dc7a
unknown <maria.g.monteros@gmail.com>
2023-11-22 09:30:22 -0300

    Listar Carrito

--- carrito.js
+++ carrito.js
@@ -8,3 +8,20 @@
 // ● Filtrar productos que coincidan con una palabra, parte de ella
 // o una letra
 // ● Eliminar producto del carrito
+
+
+let carrito = ["Gaseosa","Fideos","Arroz"]
+
+carrito.push ("Gaseosa")
+carrito.push ("Fideos")
+carrito.push ("Arroz")
+
+const listar = () => {
+   
+for (let i=0; i < carrito.lenght; i++){
+    console.log(carrito[i])
+}
+
+
+}
+
